7517b0237a795c428143fed305d113c2aca5ae6d
RHESQ <71203842+RHESQ@users.noreply.github.com>
2024-09-16 16:44:29 +0000

    Add files via upload

---
+++ ticketPriceCalculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        double ticketPrice;
+        int age;
+
+        do
+        {
+            Console.Write("Enter your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+        } while (age < 0);
+
+
+        if (age >= 65 || age <= 12)
+            ticketPrice = 7.00;
+
+        else
+            ticketPrice = 10.00;
+
+        Console.WriteLine("The ticket price is: GHC {0}", ticketPrice);
+    }
+}
---
+++ triangleTypeIdentifier/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter the length of side 1: ");
+        double side_1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the length of side 2: ");
+        double side_2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the length of side 3: ");
+        double side_3 = Convert.ToDouble(Console.ReadLine());
+
+        if (side_1 == side_2 && side_2 == side_3)
+            Console.WriteLine("Equilateral triangle");
+
+        else if (side_1 == side_2 || side_1 == side_3 || side_2 == side_3)
+            Console.WriteLine("Isosceles triangle");
+
+        else
+            Console.WriteLine("Scalene triangle");
+    }
+}
