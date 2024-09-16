6d60f8ed4c7c7f217df565a322a82dd49dba7740
RHESQ <71203842+RHESQ@users.noreply.github.com>
2024-09-16 16:22:21 +0000

    Add files via upload

---
+++ gradeCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+internal class Program
+{
+    static void Main()
+    {
+        int scoreGrade;
+        char letterGrade;
+        do
+        {
+            Console.Write("Please Enter your score 0-100: ");
+            scoreGrade = Convert.ToInt32(Console.ReadLine());
+        } while (scoreGrade is < 0 or > 100);
+
+
+        if (scoreGrade is > 90)
+            letterGrade = 'A';
+
+        else if (scoreGrade is >= 80 and <= 89)
+            letterGrade = 'B';
+
+        else if (scoreGrade is >= 70 and <= 79)
+            letterGrade = 'C';
+
+        else if (scoreGrade is >= 60 and <= 69)
+            letterGrade = 'D';
+
+        else
+            letterGrade = 'F';
+
+        Console.WriteLine(" {0} -------- {1}.", scoreGrade, letterGrade);
+    }
+}
